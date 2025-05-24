7f1e50a2e620fb0fe371a69c61a69361785311ec
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2025-05-24 16:22:31 -0400

    done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::{env, net::SocketAddr};
 
-use tokio::net::TcpListener;
+use futures::{SinkExt, StreamExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::Message;
 
 #[tokio::main]
 async fn main() {
@@ -13,4 +15,36 @@ async fn main() {
     let addr: SocketAddr = addr.parse().expect("Invalid address");
 
     let listener = TcpListener::bind(&addr).await.expect("Failed to bind");
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(async move {
+            handle_stream(stream).await;
+        });
+    }
+}
+
+async fn handle_stream(stream: TcpStream) {
+    let ws_stream = tokio_tungstenite::accept_async(stream)
+        .await
+        .expect("Error: Unable to accept tcp stream");
+
+    let (mut sender, mut receiver) = ws_stream.split();
+
+    while let Some(msg) = receiver.next().await {
+        match msg {
+            Ok(Message::Text(msg)) => {
+                let reversed = msg.chars().rev().collect::<String>();
+                sender
+                    .send(Message::Text(reversed.into()))
+                    .await
+                    .expect("Error: Unable to send message");
+            }
+            Ok(Message::Close(_)) => break,
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                break;
+            }
+        }
+    }
 }
